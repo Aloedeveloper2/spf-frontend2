5c5c5c97c592f6bb1b23ef51b4b3004cc5900c30
Aloedeveloper2 <82895671+Aloedeveloper2@users.noreply.github.com>
2021-10-04 15:05:22 +0100

    Modification of sheets

--- src/views/Sheets.vue
+++ src/views/Sheets.vue
@@ -65,7 +65,7 @@
         <v-row>
           <v-col
             v-for="item in props.items"
-            :key="item.name"
+            :key="item.poste"
             cols="12"
             sm="6"
             md="4"
@@ -73,7 +73,7 @@
           >
             <v-card>
               <v-card-title class="subheading font-weight-bold">
-                {{ item.name }}
+                {{ item.poste }}
               </v-card-title>
 
               <v-divider></v-divider>
@@ -173,118 +173,58 @@
             sortDesc: false,
             page: 1,
             itemsPerPage: 4,
-            sortBy: 'name',
+            sortBy: 'poste',
             keys: [
-            'Name',
-            'Calories',
-            'Fat',
-            'Carbs',
-            'Protein',
-            'Sodium',
-            'Calcium',
-            'Iron',
+              'Poste',
+              'Appels',
+              'Argumentaires',
+              'Commandes',
+              'RDV',
+              'Occupés',
+              'Indisponibles',
+              'Injoignables'
             ],
             items: [
-            {
-                name: 'Frozen Yogurt',
-                calories: 159,
-                fat: 6.0,
-                carbs: 24,
-                protein: 4.0,
-                sodium: 87,
-                calcium: '14%',
-                iron: '1%',
-            },
-            {
-                name: 'Ice cream sandwich',
-                calories: 237,
-                fat: 9.0,
-                carbs: 37,
-                protein: 4.3,
-                sodium: 129,
-                calcium: '8%',
-                iron: '1%',
-            },
-            {
-                name: 'Eclair',
-                calories: 262,
-                fat: 16.0,
-                carbs: 23,
-                protein: 6.0,
-                sodium: 337,
-                calcium: '6%',
-                iron: '7%',
-            },
-            {
-                name: 'Cupcake',
-                calories: 305,
-                fat: 3.7,
-                carbs: 67,
-                protein: 4.3,
-                sodium: 413,
-                calcium: '3%',
-                iron: '8%',
-            },
-            {
-                name: 'Gingerbread',
-                calories: 356,
-                fat: 16.0,
-                carbs: 49,
-                protein: 3.9,
-                sodium: 327,
-                calcium: '7%',
-                iron: '16%',
-            },
-            {
-                name: 'Jelly bean',
-                calories: 375,
-                fat: 0.0,
-                carbs: 94,
-                protein: 0.0,
-                sodium: 50,
-                calcium: '0%',
-                iron: '0%',
-            },
-            {
-                name: 'Lollipop',
-                calories: 392,
-                fat: 0.2,
-                carbs: 98,
-                protein: 0,
-                sodium: 38,
-                calcium: '0%',
-                iron: '2%',
-            },
-            {
-                name: 'Honeycomb',
-                calories: 408,
-                fat: 3.2,
-                carbs: 87,
-                protein: 6.5,
-                sodium: 562,
-                calcium: '0%',
-                iron: '45%',
-            },
-            {
-                name: 'Donut',
-                calories: 452,
-                fat: 25.0,
-                carbs: 51,
-                protein: 4.9,
-                sodium: 326,
-                calcium: '2%',
-                iron: '22%',
-            },
-            {
-                name: 'KitKat',
-                calories: 518,
-                fat: 26.0,
-                carbs: 65,
-                protein: 7,
-                sodium: 54,
-                calcium: '12%',
-                iron: '6%',
-            },
+              {
+                  poste: 'Poste 1',
+                  calls: 159,
+                  argumentaires: 6.0,
+                  commandes: 24,
+                  rdv: 4.0,
+                  occupés: 87,
+                  indisponibles: '14%',
+                  injoignables: '1%',
+              },
+              {
+                  poste: 'Poste 2',
+                  appels: 237,
+                  argumentaires: 9.0,
+                  commandes: 37,
+                  rdv: 4.3,
+                  occupés: 129,
+                  indisponibles: '8%',
+                  injoignables: '1%',
+              },
+              {
+                  poste: 'Poste 3',
+                  appels: 262,
+                  argumentaires: 16.0,
+                  commandes: 23,
+                  rdv: 6.0,
+                  occupés: 337,
+                  indisponibles: '6%',
+                  injoignables: '7%',
+              },
+              {
+                  poste: 'Poste 4',
+                  appels: 305,
+                  argumentaires: 3.7,
+                  commandes: 67,
+                  rdv: 4.3,
+                  occupés: 413,
+                  indisponibles: '3%',
+                  injoignables: '8%',
+              }
             ],
         }
         },
@@ -293,7 +233,7 @@
                 return Math.ceil(this.items.length / this.itemsPerPage)
             },
             filteredKeys () {
-                return this.keys.filter(key => key !== 'Name')
+                return this.keys.filter(key => key !== 'Poste')
             },
         },
         methods: {
