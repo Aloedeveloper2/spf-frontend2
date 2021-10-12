31b2e6555e8cca65d16133535253210cc50e7d96
Aloedeveloper2 <82895671+Aloedeveloper2@users.noreply.github.com>
2021-10-12 10:06:13 +0100

    Making some modificatiions

--- src/store/index.js
+++ src/store/index.js
@@ -33,26 +33,6 @@ export default new Vuex.Store({
       }
     }
   },
-  mutations: {
-    setUserData(state, data){
-      state.user_data = {
-        name: data.name,
-        surname: data.surname
-      }
-      /* state.user_data = {
-          id: data.id,
-          name: data.name,
-          surname: data.prenom,
-          group: data.groupe
-      }; */
-      localStorage.setItem('user_data', JSON.stringify(state.user_data));
-    },
-    initialiseStore(state){
-      if (localStorage.getItem('user_data')) {
-        state.user_data = JSON.parse(localStorage.getItem('user_data'));
-      }
-    }
-  },
   modules: {
   }
 })
