095c10f02f8c803235b0eeb1fbe09941fc2f0f2f
Aloedeveloper2 <82895671+Aloedeveloper2@users.noreply.github.com>
2021-12-21 09:03:06 +0100

    Modification of backend domain name

--- src/config/address.js
+++ src/config/address.js
@@ -1,5 +1,5 @@
 export default {
-    address: "http://localhost:5000",
+    address: "http://bissa.aloe-odoo.com/:5000",
     headers: {
         authorization: localStorage.getItem('token')
     }
