4d983edbb5b4257822e1dd68976a70e3933cc276
VanDerjoys <bissa.mintyene@gmail.com>
2022-10-13 08:04:11 +0100

    modification of backend in production url

--- src/config/address.js
+++ src/config/address.js
@@ -1,5 +1,5 @@
 export default {
-    address: process.env.NODE_ENV=="development"?"http://localhost:5000":"https://bissa.aloe-odoo.com",
+    address: process.env.NODE_ENV=="development"?"http://localhost:5000":"https://collecte.aloe-odoo.com",
     headers: {
         authorization: localStorage.getItem('token')
     }
